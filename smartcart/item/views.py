6118389c4b8e840a2d1da7ebbd38f8392a7ef529
Rafiur <orkrafiur@gmail.com>
2024-05-23 03:34:50 +0600

    added item detail page

--- smartcart/item/views.py
+++ smartcart/item/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Item
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk = pk)
+    
+    related_items = Item.objects.filter(category = item.category, is_sold = False).exclude(pk = pk)[0:3]
+    # to show related items we filter by same category and exclude the same item which is view by matching primary key
+    # the 0:3 at the end represents the split of 3 items to be shown in the related items 
+    
+    return render(request, 'item/detail.html', {
+        'item' : item,
+        'related_items' : related_items
+        
+    })
+
+
 
 # Create your views here.
